6a1495c6a5146d6d65b966bcc9fb00cb95920660
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-03-20 22:33:22 +0300

    function whose parameter is the return value from another

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,8 @@
+export default function iterativeThroughObject(reportWithIterator) {
+  const employees = [];
+  for (const r of reportWithIterator) {
+    employees.push(r);
+  }
+
+  return rmployees.join(' | ');
+}
