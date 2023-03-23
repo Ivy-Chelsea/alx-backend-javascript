ab14b665090d5e855be80056cc36c22a719261ac
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-03-23 14:30:19 +0300

    Update 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw Error('cannot divide by 0');
-  }
-
-  const result = numerator / denomnator;
-
-  return result;
+  } else {
+  return numerator / denominator;
 }
