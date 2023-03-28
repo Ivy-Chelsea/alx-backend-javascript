a510a93929f13d0d5346b66cd2c42cc5aa89a022
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-03-28 15:56:33 +0300

    specific city object array

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
-export default function getStudentsByLocation(student,, city) {
+export default function getStudentsByLocation(students, city) {
   if (students instanceof Array) {
     return students.filter((student) => student.location == city);
   }
