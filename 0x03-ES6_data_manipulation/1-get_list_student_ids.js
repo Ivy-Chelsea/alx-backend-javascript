21b8df452ce07d3466179ec997936d82a4d55b6f
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-03-28 15:26:22 +0300

    function fix

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-export default function getListStudentsIds(students) {
-  if (students instanceof Arra) {
-    return students.map(student) => student.id);
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
   }
   return [];
 }
