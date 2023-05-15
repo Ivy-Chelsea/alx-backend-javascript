c8a12be726fdc24274338c543467fb507e39257a
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-05-15 20:42:33 +0300

    display message

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Function that displays a message to STDOUT
+ * @param {string} msg: Message to display
+ */
+const displayMessage = (msg) =>
+{
+	console.log(msg);
+};
+
+module.exports = displayMessage;
