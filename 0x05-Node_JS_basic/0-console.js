8b1d1da6c8118c09b347e86a6e032020ebf04d5c
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-05-15 21:21:48 +0300

    ESlint fix

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,7 @@
  * Function that displays a message to STDOUT
  * @param {string} msg: Message to display
  */
-const displayMessage = (msg) =>
-{
+const displayMessage = (msg) => {
   console.log(msg);
 };
 
